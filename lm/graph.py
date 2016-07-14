9dc6db49c1a4d5c211dc36f73dee6cc65f632346
jg8610 <jgodwin@gmail.com>
2016-07-14 09:04:36 +0100

    awesome

--- lm/graph.py
+++ lm/graph.py
@@ -302,9 +302,9 @@ class Shared_Model(object):
             pos_prediction = tf.reshape(pos_prediction,
                 [batch_size, num_steps, pos_embedding_size])
             #pos_hidden = tf.reshape(pos_hidden, [batch_size, num_steps,
-                                    2*config.pos_decoder_size])
+                                    #2*config.pos_decoder_size])
             #chunk_hidden = tf.reshape(chunk_hidden, [batch_size, num_steps,
-                                    2*config.chunk_decoder_size])
+                                    #2*config.chunk_decoder_size])
             chunk_prediction = tf.reshape(chunk_prediction,
                 [batch_size, num_steps, chunk_embedding_size])
             lm_inputs = tf.concat(2, [chunk_prediction, pos_prediction, encoder_units])
