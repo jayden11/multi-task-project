ba17c85fdc4b4301a3cb65ce945830ef8f9a8aa8
jg8610 <jgodwin@gmail.com>
2016-07-26 17:30:43 +0100

    faster run epoch

--- lm-sentence/run_model.py
+++ lm-sentence/run_model.py
@@ -215,11 +215,18 @@ def main(model_type, dataset_path, ptb_path, save_path,
                 # print for tracking
                 print("Pos Training Accuracy After Epoch %d :  %3f" % (i+1, pos_acc))
                 print("Chunk Training Accuracy After Epoch %d : %3f" % (i+1, chunk_F1))
+                print(time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime()))
+
+                # valid_loss, posp_v, chunkp_v, lmp_v, post_v, chunkt_v, lmt_v, pos_v_loss, chunk_v_loss, lm_v_loss = \
+                #     run_epoch(session, mValid, words_v, pos_v, chunk_v,
+                #               num_pos_tags, num_chunk_tags, vocab_size, num_steps, num_batches_gold,
+                #               verbose=True, valid=True, model_type=model_type)
 
                 valid_loss, posp_v, chunkp_v, lmp_v, post_v, chunkt_v, lmt_v, pos_v_loss, chunk_v_loss, lm_v_loss = \
-                    run_epoch(session, mValid, words_v, pos_v, chunk_v,
+                    run_epoch_random.run_epoch(session, mValid,
+                              words_v, words_ptb, pos_v, pos_ptb, chunk_v, chunk_ptb,
                               num_pos_tags, num_chunk_tags, vocab_size, num_steps, num_batches_gold,
-                              verbose=True, valid=True, model_type=model_type)
+                              verbose=True,  model_type=model_type, valid=True)
 
                 # Save loss for charts
                 valid_loss_stats = np.append(valid_loss_stats, valid_loss)
@@ -340,11 +347,11 @@ def main(model_type, dataset_path, ptb_path, save_path,
 
 
             print('Getting Testing Predictions')
-            _, posp_test, chunkp_test, _, post_test, chunkt_test, _, _, _, _ = \
-                run_epoch(session, mTest,
-                          words_test, pos_test, chunk_test,
+            valid_loss, posp_test, chunkp_test, lmp_test, post_test, chunkt_test, lmt_test, pos_test_loss, chunk_test_loss, lm_test_loss = \
+                run_epoch_random.run_epoch(session, mTest,
+                          words_test, words_ptb, pos_test, pos_ptb, chunk_test, chunk_ptb,
                           num_pos_tags, num_chunk_tags, vocab_size, num_steps, num_batches_gold,
-                          verbose=True, valid=True, model_type=model_type)
+                          verbose=True,  model_type=model_type, valid=True)
 
 
             print('Writing Predictions')
